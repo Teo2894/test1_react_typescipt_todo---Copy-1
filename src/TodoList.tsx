4f7a4bcc797281436a7f65eda98293b0b2216913
Teodor Sventelius <teodor.sventelius@gmail.com>
2024-03-03 13:16:35 +0100

    Test with grid - not working

--- src/TodoList.tsx
+++ src/TodoList.tsx
@@ -1,5 +1,3 @@
-import React, { useState } from "react";
-
 /*
 Button to import text to original field
 List of notes in left field
@@ -28,8 +26,44 @@ const note2 = new Note (
 */
 
 
+import React, { useState } from "react";
+//import * as React from 'react';
+import { styled } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import Paper from '@mui/material/Paper';
+import Grid from '@mui/material/Unstable_Grid2';
+
+const Item = styled(Paper)(({ theme }) => ({
+    backgroundColor: theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+    ...theme.typography.body2,
+    padding: theme.spacing(1),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+}));
+
+export default function BasicGrid() {
+    return (
+        <Box sx={{ flexGrow: 1 }}>
+            <Grid container spacing={2}>
+                <Grid xs={8}>
+                    <Item>xs=8</Item>
+                </Grid>
+                <Grid xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid xs={4}>
+                    <Item>xs=4</Item>
+                </Grid>
+                <Grid xs={8}>
+                    <Item>xs=8</Item>
+                </Grid>
+            </Grid>
+        </Box>
+    );
+}
 
 
+/* OLD NON-GRID CODE
 export const TodoList: React.FC = () => {
     return (
         <div className="main-container">
@@ -55,9 +89,11 @@ export const TodoList: React.FC = () => {
         </div>
     );
 }
+*/
 
 
-/*
+
+/* OLD TODO LIST CODE
 interface item {
     id: number;
     text: string;
